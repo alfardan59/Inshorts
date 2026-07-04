6b93acdbbada5c92f179ffcf018566e941070fd2
alfardan <alfardanarfin500@gmail.com>
2026-07-04 10:39:13 +0530

    Update app label to 'Newisfy', add logo asset, and enhance HomeScreen UI with a navbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,22 +11,70 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final PageController controller = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: controller,
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        itemCount: newsList.length,
-        itemBuilder: (_, index) {
-          return NewsCard(
-            news: newsList[index],
-          );
-        },
+      backgroundColor: Colors.black,
+
+      body: Stack(
+        children: [
+
+          /// News Cards
+          PageView.builder(
+            controller: controller,
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            itemCount: newsList.length,
+            itemBuilder: (_, index) {
+              return NewsCard(
+                news: newsList[index],
+              );
+            },
+          ),
+
+          /// Navbar
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 18,
+                vertical: 14,
+              ),
+              child: Row(
+                children: [
+
+                  Image.asset(
+                    "assets/images/logo.png",
+                    height: 42,
+                    width: 42,
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  const Text(
+                    "Newisfy",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const Spacer(),
+
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
